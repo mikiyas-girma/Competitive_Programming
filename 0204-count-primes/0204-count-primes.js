549395faaafd4fce479a0d4abbf01cdfca79de1d
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2023-12-24 22:07:26 +0300

    Time: 279 ms (45.70%), Space: 81.9 MB (85.12%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+  let notPrimes = new Array(n);
+  for (let i = 2; i < n; i++) {
+    notPrimes[i] = false;
+  }
+  let res = 0;
+  for (let i = 2; i < n; i++) {
+    if (notPrimes[i] === false) {
+      res++;
+      for (let j = 2; i * j < n; j++) {
+        notPrimes[i * j] = true;
+      }
+    }
+  }
+  return res;
+};
