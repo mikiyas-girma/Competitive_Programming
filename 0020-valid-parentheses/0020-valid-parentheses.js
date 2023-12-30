6e06a7a8ef615a4333074c862df141388f73eadb
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2023-12-30 11:14:48 +0300

    Time: 61 ms (30.79%), Space: 42.1 MB (79.41%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+  let stack = [];
+
+  for (let c of s) {
+    // console.log(c)
+    if (c === '(' || c === '{' || c === '[') {
+      stack.push(c);
+    }
+    else {
+      if (!stack.length || (c === ')' && stack[stack.length - 1] !== '(') || (c === '}' && stack[stack.length - 1] !== '{') || (c === ']' && stack[stack.length - 1] !== '[')) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+
+  return !stack.length;
+};
