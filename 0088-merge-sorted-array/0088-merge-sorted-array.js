dee8cc09af2396db4e1c3c5421cf246b1beba967
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-01-01 22:15:17 +0300

    Time: 82 ms (5.06%), Space: 41.8 MB (77.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,7 +6,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    nums1.splice(m);
-    nums1.push(...nums2)
-    return nums1.sort((a, b) => a - b)
+  for (let i = 0; i < n && m < m + n; i++) {
+    nums1[m] = nums2[i];
+    m++;
+  }
+    return nums1.sort((a, b) => a - b);
 };
