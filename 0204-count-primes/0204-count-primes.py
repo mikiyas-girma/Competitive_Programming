b0708389cfab3ce7ebb3236e20c38223f27afb4e
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-10-03 21:43:55 +0300

    Time: 4156 ms (18.31%), Space: 226.3 MB (26.94%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        prime = [True for i in range(n+1)]
+        p = 2
+        while (p * p <= n):
+            if (prime[p] == True):
+                for i in range(p * p, n + 1, p):
+                    prime[i] = False
+            p += 1
+        count = 0
+        for p in range(2, n + 1):
+            if prime[p] and p < n:
+                count += 1
+        return count
